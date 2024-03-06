caec1ea6fd163f6b4b34702e4180acd5feec1e4a
mohhus90 <mohhus90@gmail.com>
2024-03-06 16:38:03 +0200

    emp 4

--- resources/views/admin/employees/create.blade.php
+++ resources/views/admin/employees/create.blade.php
@@ -1,12 +1,12 @@
 @extends('admin.layouts.admin')
 @section('title')
-الوظائف
+الموظفين
 @endsection
 @section('start')
-    الضبط العام
+    شئون الموظفين
 @endsection
 @section('home')
-<a href="{{ route('employees.index') }}">الوظائف</a>
+<a href="{{ route('employees.index') }}">الموظفين</a>
 
 @endsection
 @section('startpage')
@@ -17,7 +17,7 @@
 <div class="col-12">
     <div class="card">
         <div class="card-header">
-            <h3 class="card-title card_title_center">اضافة وظيفة جديد</h3>
+            <h3 class="card-title card_title_center">اضافة موظف جديد</h3>
         </div>
         <div class="card-body">
               <form method="POST" action="{{ route('employees.store') }}">
@@ -35,25 +35,75 @@
                     <div class="tab-pane fade show active" id="custom-content-below-baisc_data" role="tabpanel" aria-labelledby="custom-content-below-baisc_data-tab">
                       <br>
                       <div class="form-group form-inline">
-                        <label for="job_name" class="col-sm-2 col-form-label text-center"> اسم الوظيفة</label>
+                        <label for="employee_id" class="col-sm-2 col-form-label text-center"> كود الموظف</label>
                         <div class="col-sm-5">
-                          <input type="text" class="form-control" name="job_name" id="job_name" value="{{ old('job_name') }}" >
+                          <input type="text" class="form-control" name="employee_id" id="employee_id" value="{{ old('employee_id') }}" >
                         </div>
                       </div>
-                      @error('job_name')
+                      @error('employee_id')
                       <div class="text-danger text-center">{{ $message }}</div>
                       @enderror           
-
+                      <div class="form-group form-inline">
+                        <label for="fiinger_id" class="col-sm-2 col-form-label text-center"> كود البصمة</label>
+                          <div class="col-sm-5">
+                          <input type="text" class="form-control" name="fiinger_id" id="fiinger_id" value="{{ old('fiinger_id') }}" >
+                          </div>
+                      </div>
+                      @error('fiinger_id')
+                      <div class="text-danger text-center">{{ $message }}</div>
+                      @enderror           
+                      <div class="form-group form-inline">
+                        <label for="employee_name" class="col-sm-2 col-form-label text-center"> اسم الموظف</label>
+                          <div class="col-sm-5">
+                          <input type="text" class="form-control" name="employee_name" id="employee_name" value="{{ old('employee_name') }}" >
+                          </div>
+                      </div>
+                      @error('employee_name')
+                      <div class="text-danger text-center">{{ $message }}</div>
+                      @enderror 
+                      <div class="form-group form-inline">
+                        <label for="employee_adress" class="col-sm-2 col-form-label text-center"> اسم الموظف</label>
+                          <div class="col-sm-5">
+                          <input type="text" class="form-control" name="employee_adress" id="employee_adress" value="{{ old('employee_adress') }}" >
+                          </div>
+                      </div>
+                      @error('employee_adress')
+                      <div class="text-danger text-center">{{ $message }}</div>
+                      @enderror 
+                      <div class="form-group form-inline">
+                        <label for="emp_gender" class="col-sm-2 col-form-label text-center"> نوع الجنس</label>
+                        <select type="text" class="col-sm-3 form-select" aria-label="Disabled select example" name="type" id="type" >
+                          <option selected value="" > اختر النوع</option>
+                          <option @if (old('emp_gender')==1)selected @endif value="1" > ذكر</option>
+                          <option @if (old('emp_gender')==2)selected @endif  value="2" > انثى</option>
+                        </select>
+                      </div>
+                      @error('emp_gender')
+                      <div class="text-danger text-center">{{ $message }}</div>
+                      @enderror
+                      <div class="form-group form-inline">
+                        <label for="emp_social_status" class="col-sm-2 col-form-label text-center"> الحالة الاجتماعية</label>
+                        <select type="text" class="col-sm-3 form-select" aria-label="Disabled select example" name="type" id="type" >
+                          <option selected value="" > اختر الحالة</option>
+                          <option @if (old('emp_social_status')==1)selected @endif value="1" > اعزب</option>
+                          <option @if (old('emp_social_status')==2)selected @endif  value="2" > متزوج</option>
+                          <option @if (old('emp_social_status')==2)selected @endif  value="2" > متزوج ويعول</option>
+                        </select>
+                      </div>
+                      @error('emp_social_status')
+                      <div class="text-danger text-center">{{ $message }}</div>
+                      @enderror
                     </div>
+                  {{-- personnel_data --}}
                     <div class="tab-pane fade" id="custom-content-below-personnel_data" role="tabpanel" aria-labelledby="custom-content-below-personnel_data-tab">
                       <br>
                       <div class="form-group form-inline">
-                        <label for="job_name" class="col-sm-2 col-form-label text-center"> اسم الوظيفة</label>
+                        <label for="employee_id" class="col-sm-2 col-form-label text-center"> كود الموظف</label>
                         <div class="col-sm-5">
-                          <input type="text" class="form-control" name="job_name" id="job_name" value="{{ old('job_name') }}" >
+                          <input type="text" class="form-control" name="employee_id" id="employee_id" value="{{ old('employee_id') }}" >
                         </div>
                       </div>
-                      @error('job_name')
+                      @error('employee_id')
                       <div class="text-danger text-center">{{ $message }}</div>
                       @enderror       
                     </div>
