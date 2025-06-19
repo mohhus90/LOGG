70031ca7b67c87800934568aacc2013f41b0cbdc
mohhus90 <mohhus90@gmail.com>
2025-06-19 23:51:34 +0300

    employe . create update 6

--- app/Exports/SiftsExport.php
+++ app/Exports/SiftsExport.php
@@ -3,6 +3,7 @@
 namespace App\Exports;
 
 use App\Models\Shifts_type;
+use App\Models\Employee;
 use Maatwebsite\Excel\Concerns\FromCollection;
 
 class SiftsExport implements FromCollection
@@ -13,5 +14,16 @@ class SiftsExport implements FromCollection
     public function collection()
     {
         return Shifts_type::all();
+    }
+}
+
+class EmployeeExport implements FromCollection
+{
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    public function collection()
+    {
+        return Employee::all();
     }
 }
